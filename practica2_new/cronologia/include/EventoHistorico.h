dd326fb0422d1da16f193583be5a8d23a288b977
Miguel Anguita <maikichun@gmail.com>
2016-10-13 17:48:10 +0200

    Creando ,h

---
+++ practica2_new/cronologia/include/EventoHistorico.h
@@ -0,0 +1,64 @@
+/**
+	* @file EventoHistorico.h
+	* @brief Fichero cabecera del TDA EventoHistorico
+	*
+	*/
+	
+#ifndef __EVENTOHISTORICO
+#define __EVENTOHISTORICO
+
+#include <iostream>
+
+using namespace std;
+
+/**
+	* @brief  T.D.A EventoHistorico
+	* 
+	* Una instancia @e c del tipo de datos abstracto @c EventoHistorico
+	* es un objeto compuesto por.....
+	*
+	*
+	* @include pruebacronologia.cpp
+	* @author Miguel Anguita y Leyre Figueroa
+	* @date Octubre 2016
+	*/
+	
+class EventoHistorico{
+	
+ private:
+ 	
+/**
+	* @page Tipo Rep del TDA EventoHistorico
+	* @section Conjunto invariante de la representación
+	*
+	*
+	*
+	*
+	*/
+		
+		
+		
+	int fecha;
+	string * eventos;
+	
+ public:
+ 	
+/**
+	* @brief Constructor por defecto de la clase.
+	*/
+	
+	EventoHistorico();
+	
+	/**
+  * @brief Constructor de la clase
+  * @param anio fecha del evento histórico a construir
+  * @param 
+  * @return Crea el numero racional n/d
+  * @pre d debe ser distinto de cero
+  */		
+  
+  
+		
+		
+		
+	
