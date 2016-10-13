59e13963fe5f35832233e3957d87030a3430775b
Miguel Anguita <maikichun@gmail.com>
2016-10-13 15:55:48 +0200

    Abriendo .h del primer TDA

---
+++ EventoHistorico.h
@@ -0,0 +1,47 @@
+/**
+	* @file EventoHistorico.h
+	* @brief Fichero cabecera del TDA EventoHistorico
+	*
+	*/
+	
+#ifndef __EVENTOHISTORICO
+#define __EVENTOHISTORICO
+
+#include <iostream>
+
+using namespace std;
+
+/**
+	* @brief  T.D.A EventoHistorico
+	* 
+	* Una instancia @e c del tipo de datos abstracto @c EventoHistorico
+	* es un objeto compuesto por.....
+	*
+	*
+	* @include pruebacronologia.cpp
+	* @author Miguel Anguita y Leyre Figueroa
+	* @date Octubre 2016
+	*/
+	
+class EventoHistorico{
+	
+ private:
+ 	
+/**
+	* @page Tipo Rep del TDA EventoHistorico
+	* @section Conjunto invariante de la representación
+	*
+	*
+	*
+	*
+	*/
+		
+		
+		
+	int fecha;
+	string * eventos;
+		
+		
+		
+		
+	
